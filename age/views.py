917c132b69c4a978fdeb4227811c00958baf3171
Timur Fattakhov <maseft.flawless@yahoo.com>
2022-02-23 18:28:31 +0300

    divide values + added progress bar

--- age/views.py
+++ age/views.py
@@ -6,14 +6,5 @@ import datetime as dt
 
 
 class AgeView(View):
-    def get(self, request):
-        current_year = dt.date.today().year
-        birthday = dt.datetime(1996, 3, 20)
-        prev_birthday = dt.datetime(current_year - 1, 3, 20)
-        next_birthday = dt.datetime(current_year, 3, 20)
-
-        now = dt.datetime.now()
-        age = now.year - birthday.year - ((now.month, now.day) < (birthday.month, birthday.day))
-        past_from_bd = (now - prev_birthday).total_seconds()/(next_birthday - prev_birthday).total_seconds() #{age}.{str(past_from_bd)[2:]}
-            
+    def get(self, request):   
         return render(request, 'age/age.html')
--- static/age/js/first.js
+++ static/age/js/first.js
@@ -1,17 +1,23 @@
 let bd = new Date(1996, 2, 20);
 let current_year;
+let next_year;
 let current_year_bd;
 let age, past, next;
 let i = 0;
 let res;
 let now;
+display_age = document.querySelector('#age');
+display_dec = document.querySelector('#decimal');
+progress_bar = document.querySelector('progress')
 
 
-function start(display) {
+function start() {
     setInterval(function () {
         now = new Date();
-        current_year = now.getFullYear()
+        current_year = now.getFullYear();
+        prev_new_year = new Date(current_year, 0, 1);
         current_year_bd = new Date(current_year, 2, 20);
+
         if (now >= current_year_bd) {
             age = current_year - bd.getFullYear();
             past = current_year_bd;
@@ -22,13 +28,17 @@ function start(display) {
             next = current_year_bd;
         }
 
-        value = (age + (now - past)/(next - past));
-        display.textContent = (Math.round((value + Number.EPSILON) * (10 ** 10)) / (10 ** 10)).toFixed(10);
+        decimal = (now - past)/(next - past);
+        display_age.textContent = age;
+        display_dec.textContent = '.' + Math.round(decimal * (10 ** 10));
+
+
+        progress_bar.value = now - prev_new_year;
+        progress_bar.max = new Date(current_year + 1,0,1) - prev_new_year;
     }, 100);
 }
 
 
 window.onload = function () {
-    display = document.querySelector('#timer');
-    start(display);
+    start();
 };
